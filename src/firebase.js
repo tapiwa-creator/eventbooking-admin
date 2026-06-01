c8053e68eababab6c14d78aeeb1d006806442987
tapiwa-creator <tapiwacomfort086@gmail.com>
2026-06-01 10:50:36 +0200

    uyuh

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY?.trim(),
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN?.trim(),
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID?.trim(),
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET?.trim(),
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID?.trim(),
+    appId: process.env.REACT_APP_FIREBASE_APP_ID?.trim(),
 };
+
+console.log("FIREBASE API KEY IS:", process.env.REACT_APP_FIREBASE_API_KEY?.trim());
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
